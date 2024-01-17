cddccdf51eda1408c9e6882fdff5320e041686bb
softqwak <valentin.kuzichev@yandex.ru>
2024-01-18 02:31:13 +0300

    start

--- Src/module/parsers.py
+++ Src/module/parsers.py
@@ -30,38 +30,5 @@ class Parser:
     def now_str(self): return f"[{datetime.now()}]"
     
 
-    # ? parsing - парсит web-сайты 
-    async def parsing_web(self, parser: dict) -> dict:
-        print(f"запуск функции parsing_web to {parser}")
-        parser_reports = [
-            ['url: ...', 'title: ...']
-        ]
-        # ? здесь будут проходить этапы парсинга множества страниц на новостном ресурсе
-        await asyncio.sleep(4)
-        # ? ...
-        print(f"возвращение в функцию parsing_web to {parser}")
-        return parser_reports
-
-    # ? parsing - парсит вк
-    async def parsing_vk(self, parser: dict) -> dict:
-        print(f"запуск функции parsing_web to {parser}")
-        parser_reports = [
-            ['url: ...', 'title: ...']
-        ]
-        # ? здесь будут проходить этапы парсинга множества страниц на новостном ресурсе
-        await asyncio.sleep(4)
-        # ? ...
-        print(f"возвращение в функцию parsing_web to {parser}")
-        return parser_reports
-    
-    async def start(self, parsers):
-        tasks = [self.ioloop.create_task(self.parsing(parser=parser)) for parser in parsers]
-        
-        done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-        result = done.pop().result()
-
-        for pending_future in pending:
-            pending_future.cancel()
-
-        print(result)
+   
         
